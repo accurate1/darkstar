e52d5ad2bc77c98a2f7201d395ec7f308d799e5e
john <johnpdx1@gmail.com>
2018-05-22 14:49:43 -0400

    yay commit

--- sql/auction_house.sql
+++ sql/auction_house.sql
@@ -1,47 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 3.3.8
--- http://www.phpmyadmin.net
---
--- Serveur: localhost
--- Généré le : Sam 24 Décembre 2011 à 14:06
--- Version du serveur: 6.0.0
--- Version de PHP: 5.2.9-2
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: dspdb
+Target Host: localhost
+Target Database: dspdb
+Date: 2/16/2018 10:15:51 PM
+*/
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de données: `dspdb`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `auction_house`
---
-
-DROP TABLE IF EXISTS `auction_house`;
-CREATE TABLE IF NOT EXISTS `auction_house` (
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for auction_house
+-- ----------------------------
+CREATE TABLE `auction_house` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `itemid` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `stack` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `seller` int(10) unsigned NOT NULL DEFAULT '0',
+  `itemid` smallint(5) unsigned NOT NULL DEFAULT 0,
+  `stack` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `seller` int(10) unsigned NOT NULL DEFAULT 0,
   `seller_name` varchar(15) DEFAULT NULL,
-  `date` int(10) unsigned NOT NULL DEFAULT '0',
-  `price` int(10) unsigned NOT NULL DEFAULT '0',
+  `date` int(10) unsigned NOT NULL DEFAULT 0,
+  `price` int(10) unsigned NOT NULL DEFAULT 0,
   `buyer_name` varchar(15) DEFAULT NULL,
-  `sale` int(10) unsigned NOT NULL DEFAULT '0',
-  `sell_date` int(10) unsigned NOT NULL DEFAULT '0',
+  `sale` int(10) unsigned NOT NULL DEFAULT 0,
+  `sell_date` int(10) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `itemid` (`itemid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Contenu de la table `auction_house`
---
-
+-- ----------------------------
+-- Records 
+-- ----------------------------
